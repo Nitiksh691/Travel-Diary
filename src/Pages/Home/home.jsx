c3b9f177a8f376b87ba530667928f3ed063a6315
Nitiksh <nitikshpal@gmail.com>
2025-05-05 13:44:12 +0530

    Frontend Eastablish

---
+++ src/Pages/Home/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="text-center text-2xl font-bold p-4 bg-green-100 rounded-lg shadow-md hover:shadow-lg ">
+      Welcome to GYM Management
+    </div>
+
+
+  )
+}
+
+export default Home
